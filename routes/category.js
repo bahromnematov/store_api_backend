4f9760933803e29d854b697332c67c7a66a371b5
user <https://github.com/bahromnematov/QuizApp.git>
2024-08-06 14:00:53 +0500

    upload backend api ngrok5

--- routes/category.js
+++ routes/category.js
@@ -48,7 +48,7 @@ router.post('/', asyncHandler(async (req, res) => {
             const { name } = req.body;
             let imageUrl = 'no_url';
             if (req.file) {
-                imageUrl = `https://c323-84-54-70-19.ngrok-free.app/image/category/${req.file.filename}`;
+                imageUrl = `https://e045-84-54-70-19.ngrok-free.app/image/category/${req.file.filename}`;
             }
             console.log('url ', req.file)
 
@@ -96,7 +96,7 @@ router.put('/:id', asyncHandler(async (req, res) => {
             let image = req.body.image;
 
             if (req.file) {
-                image = `https://c323-84-54-70-19.ngrok-free.app/image/category/${req.file.filename}`;
+                image = `https://e045-84-54-70-19.ngrok-free.app/image/category/${req.file.filename}`;
             }
 
             if (!name || !image) {
--- routes/product.js
+++ routes/product.js
@@ -81,7 +81,7 @@ router.post('/', asyncHandler(async (req, res) => {
             fields.forEach((field, index) => {
                 if (req.files[field] && req.files[field].length > 0) {
                     const file = req.files[field][0];
-                    const imageUrl = `https://c323-84-54-70-19.ngrok-free.app/image/products/${file.filename}`;
+                    const imageUrl = `https://e045-84-54-70-19.ngrok-free.app/image/products/${file.filename}`;
                     imageUrls.push({ image: index + 1, url: imageUrl });
                 }
             });
@@ -146,7 +146,7 @@ router.put('/:id', asyncHandler(async (req, res) => {
             fields.forEach((field, index) => {
                 if (req.files[field] && req.files[field].length > 0) {
                     const file = req.files[field][0];
-                    const imageUrl = `https://c323-84-54-70-19.ngrok-free.app/image/products/${file.filename}`;
+                    const imageUrl = `https://e045-84-54-70-19.ngrok-free.app/image/products/${file.filename}`;
                     // Update the specific image URL in the images array
                     let imageEntry = productToUpdate.images.find(img => img.image === (index + 1));
                     if (imageEntry) {

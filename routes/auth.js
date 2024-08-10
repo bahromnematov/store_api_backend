21dd46457524abf3d75d5a691f9b954d7ebfca45
Nurbek <rustamovnurbek02@gmail.com>
2024-08-11 00:22:51 +0500

    remove User model from otp

--- routes/auth.js
+++ routes/auth.js
@@ -1,6 +1,5 @@
 const express = require('express');
 const router = express.Router();
-const User = require('../model/user');
 const OTP = require('../model/otp')
 const SMS = require('../sms')
 const otpGen = require('otp-generator')
@@ -18,11 +17,7 @@ router.post('/send-otp', async (req, res) => {
 
         await OTP.deleteMany({ phone })
 
-        const checkUser = await User.findOne({ phone })
-
-        if(!checkUser) return res.status(404).json({ success: false, message: "User not found with this phone!" });
-
-        const otp = otpGen.generate(5, {
+        const otp = otpGen.generate(4, {
             digits: true,
             lowerCaseAlphabets: false,
             upperCaseAlphabets: false,
@@ -45,10 +40,6 @@ router.post('/send-otp', async (req, res) => {
 router.post('/verify-otp', async (req, res) => {
     try {
         const { phone, otp } = req.body;
-        
-        const user = await User.findOne({ phone })
-
-        if(!user) return res.status(404).json({ success: false, message: "User not found with using this phone!" });
 
         const otpData = await OTP.findOne({ phone, otp })
 
@@ -56,7 +47,7 @@ router.post('/verify-otp', async (req, res) => {
 
         await OTP.findByIdAndUpdate(otpData._id)
 
-        res.status(200).json({ success: true, message: "OTP verify successful.", data: user });
+        res.status(200).json({ success: true, message: "OTP verify successful."});
     } catch (error) {
         res.status(500).json({ success: false, message: error.message });
     }
